72e9bda159bf7ef6535dce97070509e4474a7f2f
Eysha <N0809400@ntu.ac.uk>
2026-03-15 11:52:33 +0000

    inporting image created

--- app/gui.py
+++ app/gui.py
@@ -1,5 +1,4 @@
 import tkinter as tk    
-
 root = tk.Tk()
 #Setting window properties
 root.title("TT Corp Work Management System") #title of window
@@ -7,5 +6,12 @@ root.configure(background="white") #window background colour
 root.minsize(200, 200) # 
 root.maxsize(500, 500) # 
 root.geometry("800x500") # window size
+
+#create labels for WMS
+tk.Label(root, text = "Welcome to TT Corp Work Management System").pack()
+
+#Display an image
+image = tk.PhotoImage(file="TT Corp WMS1")
+
 
 root.mainloop()
