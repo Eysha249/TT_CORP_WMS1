5cc09d26ee3e0e2e2a399f67d69c8fcb165e3183
Eysha <N0809400@ntu.ac.uk>
2026-03-15 10:27:49 +0000

    added title and size of the window

--- app/gui.py
+++ app/gui.py
@@ -1,4 +1,11 @@
 import tkinter as tk    
 
 root = tk.Tk()
+#Setting window properties
+root.title("TT Corp Work Management System") #title of window
+root.configure(background="white") #window background colour
+root.minsize(200, 200) # 
+root.maxsize(500, 500) # 
+root.geometry("800x500") # window size
+
 root.mainloop()
