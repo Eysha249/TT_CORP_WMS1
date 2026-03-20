2f04920f25167ea8fd0db2393da0e76c763718cf
Eysha <N0809400@ntu.ac.uk>
2026-03-20 13:00:26 +0000

    database setup  create tables when the system starts

---
+++ app/database/db_setup.py
@@ -0,0 +1,40 @@
+from app.database.db_connection import get_connection # go to the database db connection file and reuse the function get_connection
+
+def create_tables(): #create database tables when the system starts
+    connection = get_connection() #calls the imported function
+    cursor = connection.cursor() #create cursor
+
+    
+    #database will create a table called users if one does not exit 
+
+    cursor.execute(""" 
+        CREATE TABLE IF NOT EXISTS users ( 
+            user_id INTGER PRIMARY KEY AUTOINCREMENT,
+            full_name TEXT NOT NULL,
+            usermane TEXT NOT NULL UNIQUE,
+            password TECT NOT NULL,
+            role TEXT NOT NULL
+        )
+    """)
+
+    connection.commit()
+    connection.close()
+
+def insert_sample_users(): #put some example users into the database
+    connection = get_connection() #call connection function sqlite3.connect ttcorp.db
+    cursor = connection.cursor() # runs SQL Commands like insert/select/creat tabels
+
+    #list that contains tuples for each column in the tabel to match fullname, username, password and role
+
+    users = [
+        ("Tom Becker", "adm1becker1", "admin123","Project Manager"),
+        ("Eysha Howell", "adm3howele","dev123","Developer")
+        ("Arshad Sher","adm3shere","dev1234","Developer")
+                 
+    ]
+
+    
+
+
+
+
