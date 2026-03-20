5a0628486569dbd1b1141c7404bffcc2166da5c6
Eysha <N0809400@ntu.ac.uk>
2026-03-20 20:49:18 +0000

    Created class for loginview success, title for login(tt corp login) and size and created a widget

--- app/gui/login_view.py
+++ app/gui/login_view.py
@@ -1,4 +1,15 @@
 import tkinter as tk # import Tkinter GUI library
-from tkinter import messagebox # import pop up dialog box
-from app.services.auth_service import login_user   
- 
+from tkinter import messagebox # import pop up dialog box to show error messages, success messages and alerts
+from app.services.auth_service import login_user  # import customer login function from login_user it will check username and password in SQLite database
+
+class LoginView: # creating a class for loginview
+    def __init__(self, root, login_success_callback):
+        self.root = root
+        self.login_success_callback = login_success_callback
+
+        self.root.title("TT Corp Login")
+        self.root.geometry ("400x300")
+
+        self.create_widgets()
+
+
