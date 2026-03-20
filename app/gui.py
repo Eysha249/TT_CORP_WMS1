67e47b1b28120d8ad37d150d75029fe538f15179
Eysha <N0809400@ntu.ac.uk>
2026-03-20 09:30:57 +0000

    Imported image from data/assets and placed image inside a label

--- app/gui.py
+++ app/gui.py
@@ -8,10 +8,13 @@ root.maxsize(500, 500) #
 root.geometry("800x500") # window size
 
 #create labels for WMS
-tk.Label(root, text = "Welcome to TT Corp Work Management System").pack()
+tk.Label(root, text = "Welcome to TT Corp Work Management System").pack() #creats label for welcome greeting
 
 #Display an image
-image = tk.PhotoImage(file="assets/Logo.png")
+image = tk.PhotoImage(file="data/assets/TT Corp Logo Image.png") # Import image from data/assets
+
+logo_label = tk.Label(root,image=image) #Place image inside a label
+logo_label.pack()
 
 
 root.mainloop()
